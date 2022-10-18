6a7805ea4333dde5a6e83dad18c18ff125ee6cb0
cdilullo <cdilullo@unm.edu>
2022-10-18 10:43:57 -0400

    Made scripts more robust and cleaned up file structure for the GitHub repo

--- plotIME.py
+++ plotIME.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy.interpolate import interp1d
 
-model = np.loadtxt('BurnsZ.txt')
-freqs, re, im = model[:,0], model[:,1], model[:,2]
-Z = re + 1j*im
-model_IME = 1.0 - np.abs( (Z-100.0)/(Z+100.0) )**2
+def main(args):
 
-intp = interp1d(freqs, model_IME, kind='cubic', bounds_error=False)
+    model = np.loadtxt('Data/BurnsZ.txt')
+    freqs, re, im = model[:,0], model[:,1], model[:,2]
+    Z = re + 1j*im
+    model_IME = 1.0 - np.abs( (Z-100.0)/(Z+100.0) )**2
 
-NS = np.loadtxt('IME_NS.txt')
-freqs_NS, IME_NS = NS[:,0], NS[:,1]
-EW = np.loadtxt('IME_EW.txt')
-freqs_EW, IME_EW = EW[:,0], EW[:,1]
+    intp = interp1d(freqs, model_IME, kind='cubic', bounds_error=False)
 
-fig, ax = plt.subplots(1,1)
-ax.set_title('Impedance Mismatch Efficiency', fontsize=16)
-ax.plot(freqs_NS/1e6, IME_NS, '-', label='NS')
-ax.plot(freqs_EW/1e6, IME_EW, '-', label='EW')
-ax.plot(freqs_NS/1e6, intp(freqs_NS/1e6), '-', label='Model')
-ax.legend(loc=0, fontsize=12)
-ax.set_xlabel('Frequency [MHz]', fontsize=12)
-ax.set_ylabel('IME', fontsize=12)
-ax.tick_params(which='both', direction='in', length=6, labelsize=12)
-plt.show()
+    freqs, IMEs = [], []
+    for file in args.files:
+        f = np.loadtxt(file)
+        
+        freq, IME = f[:,0], f[:,1]
+
+        freqs.append(freq)
+        IMEs.append(IME)
+
+    fig, ax = plt.subplots(1,1)
+    ax.set_title('Impedance Mismatch Efficiency', fontsize=16)
+    for i in range(len(args.files)):
+            ax.plot(freqs[i]/1e6, IMEs[i], '-', label='NS' if 'NS' in args.files[i] else 'EW')
+    ax.plot(freqs[0]/1e6, intp(freqs[0]/1e6), '-', label='Model')
+    ax.legend(loc=0, fontsize=12)
+    ax.set_xlabel('Frequency [MHz]', fontsize=12)
+    ax.set_ylabel('IME', fontsize=12)
+    ax.tick_params(which='both', direction='in', length=6, labelsize=12)
+    plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plot output from computeIME.py',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('files', type=str, nargs='*',
+            help='.txt files output by computeIME.py')
+
+    args = parser.parse_args()
+    main(args)
