6db660f5ed9f5b167a9eead5318166e1369077e3
cdilullo <cdilullo@unm.edu>
2022-10-19 11:36:37 -0400

    Included statistics for the other S parameters

--- FEE_stats.py
+++ FEE_stats.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 def main(args):
 
     #Read in the files and store all the data in lists.
-    freqs, S11s = [], [] 
+    freqs, S11s, S21s, S12s, S22s = [], [], [], [], []
+    Sparams = {'11': [], '21': [], '12': [], '22': []}
     for file in args.files:
         f = open(file, 'r')
         
@@ -17,7 +18,11 @@ def main(args):
             f.readline()
 
         #Read the data.
-        freq, mag_db, phase_deg = [], [], []
+        freq = [] 
+        S11_mag_db, S11_phase_deg = [], []
+        S21_mag_db, S21_phase_deg = [], []
+        S12_mag_db, S12_phase_deg = [], []
+        S22_mag_db, S22_phase_deg = [], []
         while True:
             line = f.readline()
             if line == '':
@@ -25,39 +30,66 @@ def main(args):
             l = line.split()
 
             freq.append(float(l[0]))
-            mag_db.append(float(l[1]))
-            phase_deg.append(float(l[2]))
+            S11_mag_db.append(float(l[1]))
+            S11_phase_deg.append(float(l[2]))
+            S21_mag_db.append(float(l[3]))
+            S21_phase_deg.append(float(l[4]))
+            S12_mag_db.append(float(l[5]))
+            S12_phase_deg.append(float(l[6]))
+            S22_mag_db.append(float(l[7]))
+            S22_phase_deg.append(float(l[8]))
 
         f.close()
 
         freq = np.array(freq)
-        mag_db = np.array(mag_db)
-        phase_deg = np.array(phase_deg)
-
-        #Convert magnitude from dB to linear scale
-        #and phase from degrees to radians.
-        mag = 10**(mag_db / 20.0)
-        phase = phase_deg * (np.pi/180.0)
-
-        #Build the full complex S11 parameter.
-        S11 = mag*np.exp(1j*phase)
-
         freqs.append(freq)
-        S11s.append(S11)
+        
+        S11_mag_db = np.array(S11_mag_db)
+        S11_phase_deg = np.array(S11_phase_deg)
+        S12_mag_db = np.array(S12_mag_db)
+        S12_phase_deg = np.array(S12_phase_deg)
+        S21_mag_db = np.array(S21_mag_db)
+        S21_phase_deg = np.array(S21_phase_deg)
+        S22_mag_db = np.array(S22_mag_db)
+        S22_phase_deg = np.array(S22_phase_deg)
+
+        mapper = {0: '11', 1: '21', 2: '12', 3: '22'}
+        for i, (mag_db, phase_deg) in enumerate(zip([S11_mag_db,S21_mag_db,S12_mag_db,S22_mag_db], [S11_phase_deg,S21_phase_deg,S21_phase_deg,S22_phase_deg])):
+            #Convert magnitude from dB to linear scale
+            #and phase from degrees to radians.
+            mag = 10**(mag_db / 20.0)
+            phase = phase_deg * (np.pi/180.0)
+
+            #Build the full complex S parameter and store it in the dictionary.
+            S = mag*np.exp(1j*phase)
+            Sparams[mapper[i]].append(S)
 
     #Convert to arrays and do some simple statistics.
     freqs = np.array(freqs) 
-    S11s = np.array(S11s)
+    S11s = np.array(Sparams['11'])
+    S21s = np.array(Sparams['21'])
+    S12s = np.array(Sparams['12'])
+    S22s = np.array(Sparams['22'])
 
-    #Mean, standard deviation, and min/max values.
+    #Mean and standard deviation.
     S11 = np.mean(S11s, axis=0)
-    sigma = np.std(S11s, axis=0)
-    minimum = np.min(20.0*np.log10(np.abs(S11s)), axis=0)
-    maximum = np.max(20.0*np.log10(np.abs(S11s)), axis=0)
+    S11_sigma = np.std(S11s, axis=0)
+    S21 = np.mean(S21s, axis=0)
+    S21_sigma = np.std(S21s, axis=0)
+    S12 = np.mean(S12s, axis=0)
+    S12_sigma = np.std(S12s, axis=0)
+    S22 = np.mean(S22s, axis=0)
+    S22_sigma = np.std(S22s, axis=0)
 
-    #Compute the upper and lower 1 sigma bounds on S11.
-    S11u = np.abs(S11) + sigma
-    S11l = np.abs(S11) - sigma
+    #Compute the upper and lower 1 sigma bounds for each.
+    S11u = np.abs(S11) + S11_sigma
+    S11l = np.abs(S11) - S11_sigma
+    S21u = np.abs(S21) + S21_sigma
+    S21l = np.abs(S21) - S21_sigma
+    S12u = np.abs(S12) + S12_sigma
+    S12l = np.abs(S12) - S12_sigma
+    S22u = np.abs(S22) + S22_sigma
+    S22l = np.abs(S22) - S22_sigma
     
     #Compute IMF if the antenna file was given.
     if args.ant is not None:
@@ -100,18 +132,35 @@ def main(args):
 
     #Plot the reflection coefficient of the FEE and the IMF, if computed.
     if not args.no_plot:
-        fig, ax = plt.subplots(1,1,num=1)
-        fig.suptitle(f'Mean Reflection Coefficient (N={len(args.files)})', fontsize=14)
-        ax.plot(freqs[0,:]/1e6, 20.0*np.log10(np.abs(S11)), color='k', label='Average Reflection Coefficient')
-        ax.plot(freqs[0,:]/1e6, 20*np.log10(S11u), 'r--', label=r'$1\sigma$ Bound')
-        ax.plot(freqs[0,:]/1e6, 20*np.log10(S11l), 'r--')
-        ax.fill_between(freqs[0,:]/1e6, 20*np.log10(np.abs(S11l)), 20*np.log10(np.abs(S11u)), color='r', alpha=0.25)
+        fig, axes = plt.subplots(2, 2, num=1, sharex=True)
+        axes = axes.flatten()
+        fig.suptitle(f'LWA FEE S-Parameters (N={len(args.files)})', fontsize=14)
+        for (ax, S, Su, Sl) in zip([axes[0], axes[1], axes[2], axes[3]], [S11, S21, S12, S22], [S11u, S21u, S12u, S22u], [S11l, S21l, S12l, S22l]):
+            ax.plot(freqs[0,:]/1e6, 20.0*np.log10(np.abs(S)), color='k', label='Mean')
+            ax.plot(freqs[0,:]/1e6, 20.0*np.log10(Su), 'r--', label=r'$1\sigma$ Bound')
+            ax.plot(freqs[0,:]/1e6, 20.0*np.log10(Sl), 'r--')
+            ax.fill_between(freqs[0,:]/1e6, 20.0*np.log10(Sl), 20.0*np.log10(Su), color='r', alpha=0.25)
+            ax.legend(loc=0, fontsize=12)
+            ax.tick_params(which='both', direction='in', length=6, labelsize=12)
+            if ax == axes[2] or ax == axes[3]:
+                ax.set_xlabel('Frequency [MHz]', fontsize=12)
+
+        for (ax, param) in zip([axes[0], axes[1], axes[2], axes[3]], ['S11', 'S21', 'S12', 'S22']):
+            ax.set_title(param, fontsize=12)
+            ax.set_ylabel(param+' [dB]', fontsize=12)
+
+        #fig, ax = plt.subplots(1,1,num=1)
+        #fig.suptitle(f'Mean Reflection Coefficient (N={len(args.files)})', fontsize=14)
+        #ax.plot(freqs[0,:]/1e6, 20.0*np.log10(np.abs(S11)), color='k', label='Average Reflection Coefficient')
+        #ax.plot(freqs[0,:]/1e6, 20*np.log10(S11u), 'r--', label=r'$1\sigma$ Bound')
+        #ax.plot(freqs[0,:]/1e6, 20*np.log10(S11l), 'r--')
+        #ax.fill_between(freqs[0,:]/1e6, 20*np.log10(np.abs(S11l)), 20*np.log10(np.abs(S11u)), color='r', alpha=0.25)
         #ax.plot(freqs[0,:]/1e6, 20*np.log10(minimum), 'k--')
         #ax.plot(freqs[0,:]/1e6, 20*np.log10(maximum), 'k--')
-        ax.legend(loc=0, fontsize=12)
-        ax.set_xlabel('Frequency [MHz]', fontsize=12)
-        ax.set_ylabel(r'$\Gamma$ [dB]', fontsize=12)
-        ax.tick_params(which='both', direction='in', length=6, labelsize=12)
+        #ax.legend(loc=0, fontsize=12)
+        #ax.set_xlabel('Frequency [MHz]', fontsize=12)
+        #ax.set_ylabel(r'$\Gamma$ [dB]', fontsize=12)
+        #ax.tick_params(which='both', direction='in', length=6, labelsize=12)
 
         try:
             fig, ax = plt.subplots(1,1,num=2)
